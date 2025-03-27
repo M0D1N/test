ec0c0c2eb31c870a5b57e67e9e5323439e583440
M0D1N <81459885+M0D1N@users.noreply.github.com>
2025-03-27 17:13:07 +0900

    Add files via upload

---
+++ test_pack.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "name": "TestPack",
+    "author": "M0D1N",
+    "ProfileLink": "https://playentry.org/profile/6026539845673d648281f982",
+    "Category": "test",
+    "PackIcon": "",
+    "CategoryIcon": "",
+
+    "blocks": [
+        {
+        "template": "콘솔에 %1출력하기 %2",
+        "type": "console_log",
+        "class": "entry",
+        "skeleton": "basic",
+        "color": [ "#303030", "#000000", "#ffffff" ],
+        "statements": [],
+        "events": {},
+        "params": [
+            {
+                "type": "Block",
+                "accept": "string",
+                "defaultType": "number",
+                "defaultValue": "Hello, World!",
+                "key": "VALUE"
+            },
+            {
+                "type": "Indicator",
+                "img": "block_icon/moving_icon.svg",
+                "size": 11
+            }
+        ],
+        "py": "Entry.console_log(%1)",
+        "func": function(sprite, script) {
+            const value = script.getStringValue('VALUE', script);
+            return console.log(value)
+        },
+        "isFor": [],
+        "isNotFor": []
+        }
+]
+}
