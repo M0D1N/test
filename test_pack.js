2d2b35a5fb3c87fb6c6ec157882c05b8168de516
M0D1N <81459885+M0D1N@users.noreply.github.com>
2025-03-30 20:48:09 +0900

    Update test_pack.js

--- test_pack.js
+++ test_pack.js
@@ -6,7 +6,7 @@ export var pack = {
     "PackIcon": "",
     "Category": "test",
     "CategoryName": "테스트",
-    "CategoryColor": "#123456",
+    "CategoryColor": "#ff8254",
     "CategoryIcon": {
         "BlockIcon": "../../../../../uploads/e8/49/e8494736m1p50q5a2n6nee8421e0kqg5.png",
         "CategoryIcon": [
