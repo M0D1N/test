55f43bf5ec11c5c37f9afbcfa8b825e656f333b5
M0D1N <81459885+M0D1N@users.noreply.github.com>
2025-03-30 18:48:19 +0900

    Update test_pack.js

--- test_pack.js
+++ test_pack.js
@@ -6,7 +6,7 @@ export var pack = {
     "PackIcon": "",
     "Category": "test",
     "CategoryName": "테스트",
-    "CategoryIcon": "../../../uploads/e8/49/e8494736m1p50q5a2n6nee8421e0kqg5.png",
+    "CategoryIcon": "../../../../../uploads/e8/49/e8494736m1p50q5a2n6nee8421e0kqg5.png",
     "CategoryBlockIcon": "../../../uploads/e8/49/e8494736m1p50q5a2n6nee8421e0kqg5.png"
 }
     
